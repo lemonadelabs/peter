8c78d4783f1b3defa7c1dd1499402c22b34ef9ba
Caleb Sawtell <caleb.sawtell@gmail.com>
2015-11-09 17:20:09 +1300

    #3 pages for password reset and forgotten password

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,16 @@ function addListeners() {
     event.preventDefault();
     login();
   });
+
+  $( "#forgot-form" ).submit(function( event ) {
+    event.preventDefault();
+    forgotPassword();
+  });
+
+  $( "#reset-form" ).submit(function( event ) {
+    event.preventDefault();
+    resetPassword();
+  });
 }
 
 function login(){
@@ -52,4 +62,122 @@ function login(){
       $(".login-message").html("Could not find login server.");
     }
   });
+}
+
+function resetPassword(){
+  var formError = false;
+  var password1 = $("input[name='password1']").val();
+  var password2 = $("input[name='password2']").val();
+
+
+  if(password1 !== password2){
+    $(".login-message").html("Passwords don't match.");
+    formError = true;
+  }
+  else if (password1 === "" && password2 === "") {
+    $(".login-message").html("Passwords not set.");
+    formError = true;
+  }
+  else{
+    formError = false;
+  }
+
+  if(formError){
+    $("#login-form").removeClass('denied').width(); // reading width() forces reflow
+    $("#login-form").addClass('denied');
+    return;
+  }
+
+  var newPasswordHash = sha1(password1);
+  var token = getQueryVariable("token");
+  console.log(token);
+  if (token === undefined || token === false) {
+    //hopefully this doesn't happen.
+    $(".login-message").html("Could not find token, try following the link in your email again.");
+    return;
+  }
+
+  $.post("/api/resetpassword",
+    {
+      newPasswordHash:newPasswordHash,
+      token:token
+    })
+  .done(function(data, status){
+    //move the browser location to the new url
+    $(".login-message").html("Password Reset.");
+  })
+  .fail(function(data){
+    if(data.status === 403){
+      $(".login-message").html("Username or password invalid.");
+      $("#login-form").removeClass('denied').width(); // reading width() forces reflow
+      $("#login-form").addClass('denied');
+    }
+    if(data.status === 404){
+      $(".login-message").html("Could not find login server.");
+    }
+  });
+}
+
+function forgotPassword(){
+  var formError = false;
+  var username = $("input[name='username']").val();
+  var email = $("input[name='email']").val();
+
+  //$("#forgot-form").fadeOut();
+
+   if (email===""&&username==="") {
+    $(".login-message").html("We need your username & email to reset your password.");
+    $("input[name='email']").addClass("inputError");
+    $("input[name='username']").addClass("inputError");
+    formError = true;
+  }
+  else if(username===""){
+    $(".login-message").html("We need your username to reset your password.");
+    $("input[name='username']").addClass("inputError");
+    formError = true;
+  }
+  else if (email==="") {
+    $(".login-message").html("We need your email to reset your password.");
+    $("input[name='email']").addClass("inputError");
+    formError = true;
+  }
+  else {
+    formError = false;
+  }
+
+  if(formError){
+    $("#login-form").removeClass('denied').width(); // reading width() forces reflow
+    $("#login-form").addClass('denied');
+    return;
+  }
+
+  $.post("/api/resetrequest",
+    {
+      username:username,
+      email:email
+    })
+  .done(function(data, status){
+    //move the browser location to the new url
+    $(".login-message").html("Email sent to "+email);
+  })
+  .fail(function(data){
+    if(data.status === 400){
+      $(".login-message").html("Something went wrong, please contact <a href='mailto:support@lemonadelabs.io'>support</a>.");
+    }
+    if(data.status === 404){
+      $(".login-message").html("Could not find login server.");
+    }
+  });
+}
+
+//taken from https://css-tricks.com/snippets/javascript/get-url-variables/
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
